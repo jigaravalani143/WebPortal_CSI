cbd241e89bd3053c58ca090686b9c26a1d5fe333
Jigar <jigaravalani143@gmail.com>
2019-09-21 23:13:36 +0530

    changes

---
+++ src/components/start.vue
@@ -0,0 +1,265 @@
+<template>
+
+
+<div id="app">
+  <v-app light>
+   <v-toolbar class="black">
+    <img src="http://djcsi.co.in/images/logo.png" alt="Vuetify.js" height="40">
+
+   </v-toolbar>
+   <v-content>
+     <section>
+       <v-parallax src="https://www.htmlcsscolor.com/preview/gallery/00FFFF.png" height="600">
+         <v-layout
+           column
+           align-center
+           justify-center
+           class="white--text"
+         >
+           <img src="http://djcsi.co.in/images/cube1.png" alt="Vuetify.js" height="200">
+           <br />
+          <h1 class="black--text mb-2 display-1 text-xs-center">Welcome to DJCSI's Web-Portal</h1>
+        <!--   <div class="subheading mb-3 black--text text-xs-center">DJCSI represents Computer Society of India at the college level and it is affiliated with Dwarkadas J. Sanghvi College of Engineering.
+</div>-->
+          <br />
+          <br />
+          <br /><v-btn
+             class="blue lighten-2 mt-5"
+             dark
+             large
+             href="/Login"
+           >
+             Login to Get Started
+           </v-btn>
+         </v-layout>
+       </v-parallax>
+     </section>
+
+     <section>
+       <v-layout
+         column
+         wrap
+         class="my-5"
+         align-center
+       >         <v-flex xs12>
+           <v-container grid-list-xl>
+             <v-layout row wrap align-center>
+               <v-flex xs12 md4>
+                 <v-card class="elevation-0 transparent">
+                   <v-card-text class="text-xs-center">
+                     <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                   </v-card-text>
+                   <v-card-title primary-title class="layout justify-center">
+                     <div class="headline text-xs-center">capitalize</div>
+                   </v-card-title>
+                   <v-card-text>
+              CSI attempts to capitalize on the abundant talent
+               and ideas and give others a platform to showcase their perceptions.     </v-card-text>
+                 </v-card>
+               </v-flex>
+               <v-flex xs12 md4>
+                 <v-card class="elevation-0 transparent">
+                   <v-card-text class="text-xs-center">
+                     <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                   </v-card-text>
+                   <v-card-title primary-title class="layout justify-center">
+                     <div class="headline">stimulate</div>
+                   </v-card-title>
+                   <v-card-text>
+                    CSI aspires to stimulate,
+                    spark new ideas and ignite the concept of innovation amongst today's generation.
+                   </v-card-text>
+                 </v-card>
+               </v-flex>
+               <v-flex xs12 md4>
+                 <v-card class="elevation-0 transparent">
+                   <v-card-text class="text-xs-center">
+                     <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                   </v-card-text>
+                   <v-card-title primary-title class="layout justify-center">
+                     <div class="headline text-xs-center">Inspire</div>
+                   </v-card-title>
+                   <v-card-text>
+CSI aims to inspire, act and engage and truly believes in the precept ,
+ "To be inspired is great, to inspire is incredible
+                   </v-card-text>
+                 </v-card>
+               </v-flex>
+             </v-layout>
+           </v-container>
+         </v-flex>
+       </v-layout>
+     </section>
+
+  <!--   <section>
+       <v-parallax src="assets/section.jpg" height="380">
+         <v-layout column align-center justify-center>
+           <div class="headline black--text mb-3 text-xs-center">Web development has never been easier</div>
+           <em>Kick-start your application today</em>
+           <v-btn
+             class="blue lighten-2 mt-5"
+             dark
+             large
+             href="/pre-made-themes"
+           >
+             Get Started
+           </v-btn>
+         </v-layout>
+       </v-parallax>
+     </section>
+-->
+     <section>
+       <v-container grid-list-xl>
+         <v-layout row wrap justify-center class="my-5">
+           <v-flex xs12 sm4>
+             <v-card class="elevation-0 transparent">
+               <v-card-title primary-title class="layout justify-center">
+                 <div class="headline"> DJ CSI</div>
+               </v-card-title>
+               <v-card-text>
+DJCSI represents Computer Society of India at the college level and it is affiliated with Dwarkadas J. Sanghvi College of Engineering.
+               </v-card-text>
+             </v-card>
+           </v-flex>
+           <v-flex xs12 sm4 offset-sm1>
+             <v-card class="elevation-0 transparent">
+               <v-card-title primary-title class="layout justify-center">
+                 <div class="headline">Contact us</div>
+               </v-card-title>
+
+               <v-list class="transparent">
+              <!--    <v-list-tile>
+                   <v-list-tile-action>
+                    <v-icon class="blue--text text--lighten-2">phone</v-icon>
+                   </v-list-tile-action>
+                   <v-list-tile-content>
+                     <v-list-tile-title>777-867-5309</v-list-tile-title>
+                   </v-list-tile-content>
+                 </v-list-tile> -->
+                 <v-list-tile>
+                   <v-list-tile-action>
+                     <v-icon class="blue--text text--lighten-2">place</v-icon>
+                   </v-list-tile-action>
+                   <v-list-tile-content>
+                     <v-list-tile-title>DJSCE Vileparle, Mumbai</v-list-tile-title>
+                   </v-list-tile-content>
+                 </v-list-tile>
+                 <v-list-tile>
+                   <v-list-tile-action>
+                     <v-icon class="blue--text text--lighten-2">email</v-icon>
+                   </v-list-tile-action>
+                   <v-list-tile-content>
+                     <v-list-tile-title>djcsi1718@gmail.com</v-list-tile-title>
+                   </v-list-tile-content>
+                 </v-list-tile>
+               </v-list>
+             </v-card>
+           </v-flex>
+         </v-layout>
+       </v-container>
+     </section>
+
+
+     <section>
+       <v-layout
+         column
+         wrap
+         class="my-5"
+         align-center
+       >
+      <v-flex xs12 sm4 class="my-3">
+         <div class="text-xs-center">
+         <h2 class="headline">Get our App </h2>
+         <span class="subheading">
+         <br >
+         <a href="https://play.google.com/store/apps/details?id=com.djcsi.csiapp&hl=en"><img src="https://cdn-images-1.medium.com/max/1600/1*OIIv4FEjJQMqh-zEPhtlYA.png" alt="Vuetify.js" height="100">
+         </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <a href="https://itunes.apple.com/us/app/djcsi/id1330145865?mt=8"><img src="https://upload.wikimedia.org/wikipedia/commons/5/5d/Available_on_the_App_Store_%28black%29.png" alt="Vuetify.js" height="100">
+          </a>
+           </span>
+         </div>
+       </v-flex>
+</v-layout>
+</section>
+
+     <v-footer class="blue darken-2">
+       <v-layout row wrap align-center>
+         <v-flex xs12>
+           <div class="white--text ml-3">
+             designed with
+             <v-icon class="red--text">favorite</v-icon>
+             by <a class="white--text" href="http://djcsi.co.in/" target="_blank">DJCSI's Technical Team</a>
+           </div>
+         </v-flex>
+       </v-layout>
+     </v-footer>
+   </v-content>
+ </v-app>
+</div>
+
+
+</template>
+
+<script>
+
+
+
+import firebase from 'firebase';
+
+
+export default {
+  components:{
+
+  },
+
+data(){
+      return{
+expired:[],
+expired_poll:[],
+color:'#00FFFF',
+title: 'Your Logo'
+            }
+      },
+      methods:{
+
+
+    },
+
+//code for expired nodes of quiz and polls will come her which is stored in expirednodes.txt
+
+      }
+
+
+</script>
+
+<style>
+
+
+
+#home-block{
+  }
+
+
+label{
+display: block;
+margin: 20px 0 10px;
+}
+
+input[type="text"] {
+display: block;
+width: 100%;
+padding: 8px;
+}
+
+#preview{
+padding: 10px 20px;
+border: 1px dotted #ccc;
+margin:30px 0;
+}
+
+h3
+{
+margin-top:10px;
+}
+
+</style>
